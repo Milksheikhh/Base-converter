4d377083a5c6a4cd52d7cb1d541ecd72c474bd7b
S1lenc3XD <144755002+S1lenc3XD@users.noreply.github.com>
2024-06-04 20:18:07 -0500

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,5 +1,5 @@
 while True:
-    #Get from base for conversion
+    # Get the base to convert from
     while True:
         validfrombases = ("binary", "hexadecimal", "octal", "decimal")
         frombase = input("Would you like to convert from binary, decimal, hexadecimal or octal? ").lower()
@@ -8,11 +8,12 @@ while True:
         else:
             break
 
-    #Binary
-    if (frombase == "Binary" or frombase == "binary"):
+    # Conversion from binary
+    if frombase == "binary":
         while True:
             binaryinput = input("Please enter your binary number: ")
             try:
+                # Convert binary to decimal, octal, and hexadecimal
                 decimalfrombin = int(binaryinput, 2)
                 octfrombin = oct(decimalfrombin)
                 hexfrombin = hex(decimalfrombin)
@@ -25,11 +26,12 @@ while True:
         print("octal -", octfrombin)
         print("hexadecimal -", hexfrombin)
 
-    #Decimal
-    if (frombase == "Decimal" or frombase == "decimal"):
+    # Conversion from decimal
+    if frombase == "decimal":
         while True:
             decimalinput = input("Please enter your decimal number: ")
             try:
+                # Convert decimal to binary, octal, and hexadecimal
                 decimalinput = int(decimalinput)
                 binfromdecimal = bin(decimalinput)
                 octfromdecimal = oct(decimalinput)
@@ -38,55 +40,57 @@ while True:
             except ValueError:
                 print("Invalid input please enter a decimal number")
         print()
-        print("Your binary number in:")
+        print("Your decimal number in:")
         print("binary -", binfromdecimal)
         print("octal -", octfromdecimal)
         print("hexadecimal -", hexfromdecimal)
 
-    #Octal
-    if (frombase == "Octal" or frombase == "octal"):
+    # Conversion from octal
+    if frombase == "octal":
         while True:
             octalinput = input("Please enter your octal number: ")
             try:
+                # Convert octal to decimal, binary, and hexadecimal
                 decimalfromoct = int(octalinput, 8)
                 binfromoct = bin(decimalfromoct)
                 hexfromoct = hex(decimalfromoct)
                 break
             except ValueError:
-                print("Invalid input please enter a decimal number")
+                print("Invalid input please enter a valid octal number")
         print()
         print("Your octal number in:")
         print("decimal -", decimalfromoct)
         print("binary -", binfromoct)
         print("hexadecimal -", hexfromoct)
 
-    #Hexadecimal
-    if (frombase == "Hexadecimal" or frombase == "hexadecimal"):
+    # Conversion from hexadecimal
+    if frombase == "hexadecimal":
         while True:
             hexinput = input("Please enter your hexadecimal number: ")
             try:
+                # Convert hexadecimal to decimal, binary, and octal
                 decimalfromhex = int(hexinput, 16)
                 binfromhex = bin(decimalfromhex)
                 octfromhex = oct(decimalfromhex)
                 break
             except ValueError:
-                print("Invalid input please enter a decimal number")
+                print("Invalid input please enter a valid hexadecimal number")
         print()
-        print("Your octal number in:")
+        print("Your hexadecimal number in:")
         print("decimal -", decimalfromhex)
         print("binary -", binfromhex)
         print("octal -", octfromhex)
     
-    #Asks the user if they want to re-run the pogram
+    # Ask the user if they want to re-run the program
     print()
     while True:
         validrepeatinput = ("yes", "no", "y", "n")
-        repeat = input("Would you like to run the pogram again? ").lower()
+        repeat = input("Would you like to run the program again? ").lower()
         if repeat not in validrepeatinput:
-            print("Invalid in put please enter \"yes\", \"no\", \"y\" or \"n\"")
+            print("Invalid input please enter \"yes\", \"no\", \"y\" or \"n\"")
         else:
             break
-    if (repeat == "yes" or repeat == "y"):
-            print()
+    if repeat == "yes" or repeat == "y":
+        print()
     else:
         break
